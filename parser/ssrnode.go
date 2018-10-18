7a984d6615f2aec34dd4b9120ddc07b2a11447e5
apocelipes <seve3r@outlook.com>
2018-10-18 18:38:48 +0800

    增加SSRNode的Load方法

--- parser/ssrnode.go
+++ parser/ssrnode.go
@@ -3,6 +3,7 @@ package parser
 import (
 	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"os"
 )
 
@@ -26,7 +27,7 @@ type SSRNode struct {
 	Minx string `json:"obfs"`
 }
 
-// Store 讲配置信息存入json文件
+// Store 将配置信息存入json文件
 func (s *SSRNode) Store(path string) error {
 	f, err := os.OpenFile(path, os.O_WRONLY|os.O_CREATE, 0664)
 	if err != nil {
@@ -40,6 +41,27 @@ func (s *SSRNode) Store(path string) error {
 	}
 
 	if _, err := f.Write(data); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// Load 从配置文件读取node信息
+func (s *SSRNode) Load(path string) error {
+	f, err := os.Open(path)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+
+	data, err := ioutil.ReadAll(f)
+	if err != nil {
+		return err
+	}
+
+	err = json.Unmarshal(data, s)
+	if err != nil {
 		return err
 	}
 
