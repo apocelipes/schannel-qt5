b772e329e431f4e93cba348e7fd26c3dd6e84e83
apocelipes <seve3r@outlook.com>
2018-10-25 22:26:11 +0800

    add NodeSelectDialog and NodeDetailWidget

---
+++ widgets/ssr_node_detail_widget.go
@@ -0,0 +1,111 @@
+package widgets
+
+import (
+	"fmt"
+
+	"github.com/therecipe/qt/widgets"
+
+	"schannel-qt5/parser"
+)
+
+// NodeDetailWidget 显示节点的详细信息
+type NodeDetailWidget struct {
+	widgets.QWidget
+
+	// 节点名称
+	name *widgets.QLabel
+	// 代理类型
+	proxyType *widgets.QLabel
+	// IP和端口信息
+	ip       *widgets.QLabel
+	port     *widgets.QLabel
+	password *widgets.QLabel
+	// 加密算法
+	crypt *widgets.QLabel
+	// 混淆算法
+	mixin *widgets.QLabel
+	// 连接协议
+	proto *widgets.QLabel
+	// 服务器地理信息
+	geo *widgets.QLabel
+}
+
+// NewNodeDetailWidgetWithNode 根据参数给出的节点显示其详细信息
+func NewNodeDetailWidgetWithNode(node *parser.SSRNode) *NodeDetailWidget {
+	n := NewNodeDetailWidget(nil, 0)
+
+	n.InitUI()
+	n.SetNodeDetail(node)
+
+	return n
+}
+
+// InitUI 初始化UI
+func (n *NodeDetailWidget) InitUI() {
+	mainLayout := widgets.NewQGridLayout2()
+	title := widgets.NewQLabel2("节点信息", nil, 0)
+	mainLayout.AddWidget3(title, 0, 0, 1, 2, 0)
+
+	nameLabel := widgets.NewQLabel2("节点名称：", nil, 0)
+	n.name = widgets.NewQLabel(nil, 0)
+	mainLayout.AddWidget(nameLabel, 1, 0, 0)
+	mainLayout.AddWidget(n.name, 1, 1, 0)
+
+	proxyLabel := widgets.NewQLabel2("代理类型：", nil, 0)
+	n.proxyType = widgets.NewQLabel(nil, 0)
+	mainLayout.AddWidget(proxyLabel, 2, 0, 0)
+	mainLayout.AddWidget(n.proxyType, 2, 1, 0)
+
+	ipLabel := widgets.NewQLabel2("IP：", nil, 0)
+	n.ip = widgets.NewQLabel(nil, 0)
+	mainLayout.AddWidget(ipLabel, 3, 0, 0)
+	mainLayout.AddWidget(n.ip, 3, 1, 0)
+	portLabel := widgets.NewQLabel2("端口：", nil, 0)
+	n.port = widgets.NewQLabel(nil, 0)
+	mainLayout.AddWidget(portLabel, 4, 0, 0)
+	mainLayout.AddWidget(n.port, 4, 1, 0)
+	passwordLabel := widgets.NewQLabel2("密码：", nil, 0)
+	n.password = widgets.NewQLabel(nil, 0)
+	mainLayout.AddWidget(passwordLabel, 5, 0, 0)
+	mainLayout.AddWidget(n.password, 5, 1, 0)
+
+	cryptLabel := widgets.NewQLabel2("加密算法：", nil, 0)
+	n.crypt = widgets.NewQLabel(nil, 0)
+	mainLayout.AddWidget(cryptLabel, 6, 0, 0)
+	mainLayout.AddWidget(n.crypt, 6, 1, 0)
+
+	mixinLabel := widgets.NewQLabel2("混淆算法：", nil, 0)
+	n.mixin = widgets.NewQLabel(nil, 0)
+	mainLayout.AddWidget(mixinLabel, 7, 0, 0)
+	mainLayout.AddWidget(n.mixin, 7, 1, 0)
+
+	protoLabel := widgets.NewQLabel2("连接协议：", nil, 0)
+	n.proto = widgets.NewQLabel(nil, 0)
+	mainLayout.AddWidget(protoLabel, 8, 0, 0)
+	mainLayout.AddWidget(n.proto, 8, 1, 0)
+
+	geoLabel := widgets.NewQLabel2("国家/地区：", nil, 0)
+	n.geo = widgets.NewQLabel(nil, 0)
+	mainLayout.AddWidget(geoLabel, 9, 0, 0)
+	mainLayout.AddWidget(n.geo, 9, 1, 0)
+
+	n.SetLayout(mainLayout)
+}
+
+// SetNodeDetail 设置需要显示详细信息的节点
+func (n *NodeDetailWidget) SetNodeDetail(node *parser.SSRNode) {
+	if node == nil {
+		return
+	}
+
+	n.name.SetText(node.NodeName)
+	n.proxyType.SetText(node.Type)
+	n.ip.SetText(node.IP)
+	port := fmt.Sprintf("%v", node.Port)
+	n.port.SetText(port)
+	n.password.SetText(node.Passwd)
+	n.crypt.SetText(node.Crypto)
+	n.mixin.SetText(node.Minx)
+	n.proto.SetText(node.Proto)
+	n.geo.SetText(getGeoName(node.NodeName))
+}
---
+++ widgets/ssr_node_select_dialog.go
@@ -0,0 +1,100 @@
+package widgets
+
+import (
+	"github.com/therecipe/qt/widgets"
+
+	"schannel-qt5/parser"
+)
+
+// NodeSelectDialog 显示所有节点信息，并选择设置节点
+// 需要使用模态运行
+type NodeSelectDialog struct {
+	widgets.QDialog
+
+	// dialog功能按钮
+	okButton, cancelButton *widgets.QPushButton
+	// 节点列表
+	list *widgets.QListWidget
+	// node详细信息
+	detail *NodeDetailWidget
+
+	// 选择的节点，将作为结果被使用
+	CurrentNode *parser.SSRNode
+	// 全部可用节点，已排序
+	nodes []*parser.SSRNode
+}
+
+// NewNodeSelectDialog2 生成node选择对话框
+func NewNodeSelectDialog2(current *parser.SSRNode, nodes []*parser.SSRNode) *NodeSelectDialog {
+	if nodes == nil {
+		return nil
+	}
+
+	dialog := NewNodeSelectDialog(nil, 0)
+	dialog.CurrentNode = current
+	// dialog运行于模态，nodes不会被修改
+	dialog.nodes = nodes
+	dialog.InitUI()
+
+	return dialog
+}
+
+// InitUI 初始化界面
+func (dialog *NodeSelectDialog) InitUI() {
+	dialog.detail = NewNodeDetailWidgetWithNode(dialog.CurrentNode)
+	dialog.list = widgets.NewQListWidget(nil)
+	dialog.list.AddItems(dialog.getNodeNames())
+	// 绑定选择事件
+	dialog.list.ConnectCurrentRowChanged(func(index int) {
+		// 因为list和nodes顺序一致，所以可以直接找到当前选中节点
+		if index == -1 {
+			return
+		}
+		dialog.CurrentNode = dialog.nodes[index]
+		dialog.detail.SetNodeDetail(dialog.CurrentNode)
+	})
+	// 设置当前选择的节点
+	if dialog.CurrentNode != nil {
+		for i := range dialog.nodes {
+			if dialog.nodes[i].NodeName == dialog.CurrentNode.NodeName {
+				dialog.list.SetCurrentRow(i)
+				break
+			}
+		}
+	}
+
+	listLayout := widgets.NewQVBoxLayout()
+	listLabel := widgets.NewQLabel2("可用节点列表：", nil, 0)
+	listLayout.AddWidget(listLabel, 0, 0)
+	listLayout.AddWidget(dialog.list, 0, 0)
+
+	dialog.okButton = widgets.NewQPushButton2("选择", nil)
+	dialog.okButton.ConnectClicked(func(_ bool) {
+		dialog.Accept()
+	})
+	dialog.cancelButton = widgets.NewQPushButton2("取消", nil)
+	dialog.cancelButton.ConnectClicked(func(_ bool) {
+		dialog.Reject()
+	})
+
+	mainLayout := widgets.NewQGridLayout2()
+	mainLayout.AddLayout(listLayout, 0, 0, 0)
+	mainLayout.AddWidget3(dialog.detail, 0, 1, 1, 2, 0)
+	// 水平分割线
+	vFrame := widgets.NewQFrame(nil, 0)
+	vFrame.SetFrameShape(widgets.QFrame__VLine)
+	mainLayout.AddWidget3(vFrame, 1, 0, 1, 3, 0)
+	mainLayout.AddWidget(dialog.cancelButton, 2, 1, 0)
+	mainLayout.AddWidget(dialog.okButton, 2, 2, 0)
+	dialog.SetLayout(mainLayout)
+}
+
+// getNodeNames 返回所有节点的名称，顺序与nodes一致
+func (dialog *NodeSelectDialog) getNodeNames() []string {
+	names := make([]string, 0, 10)
+	for _, v := range dialog.nodes {
+		names = append(names, v.NodeName)
+	}
+
+	return names
+}
