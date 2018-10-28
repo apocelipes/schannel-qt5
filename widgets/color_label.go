03c47bbe18a5fc330bccd85b0ed4e4b92d42f8b1
apocelipes <seve3r@outlook.com>
2018-10-28 16:04:52 +0800

    重构ColorLabel，使用qss

--- widgets/color_label.go
+++ widgets/color_label.go
@@ -1,147 +1,83 @@
 package widgets
 
 import (
-	"errors"
 	"fmt"
-	"regexp"
 
 	"github.com/therecipe/qt/widgets"
 )
 
-const (
-	red    = `<font color="#ff0000">%s</font>`
-	orange = `<font color="#ff8000">%s</font>`
-	yellow = `<font color="#ffff00">%s</font>`
-	green  = `<font color="#00ff00">%s</font>`
-	blue   = `<font color="#0080ff">%s</font>`
-	indigo = `<font color="#0000a0">%s</font>`
-	purple = `<font color="#8000ff">%s</font>`
-	gray   = `<font color="#c0c0c0">%s</font>`
-)
-
 var (
-	colors map[string]string = map[string]string{
-		"red":    red,
-		"orange": orange,
-		"blue":   blue,
-		"yellow": yellow,
-		"green":  green,
-		"indigo": indigo,
-		"purple": purple,
-		"gray":   gray,
-	}
-	textMatcher = regexp.MustCompile(`<font color=".+">(.+)</font>`)
-	// 找不到对应颜色
-	ErrColorNotFound = errors.New("cannot found text")
+	// 控制颜色的qss模板
+	colorStyle = "QLabel{color:%s;}"
+	// 默认颜色-黑色
+	defaultStyle = "QLabel{color:black;}"
 )
 
 // ColorLabel 使用HTML显示彩色文字
 type ColorLabel struct {
 	widgets.QLabel
 
+	// color style sheet
 	defaultColor string
 }
 
 // NewColorLabelWithColor 生成colorlabel，设置default color为color
 // color为空则设置为黑色
+// color可以是颜色对应的名字，例如"black", "green"
+// 也可以是16进制的RGB值，例如 #ffffff, #ff08ff, #000000
 func NewColorLabelWithColor(text, color string) *ColorLabel {
 	l := NewColorLabel(nil, 0)
 
-	if err := l.SetDefaultColor(color); err != nil {
-		return nil
-	}
+	l.SetDefaultColor(color)
 	l.SetDefaultColorText(text)
 
 	return l
 }
 
-// getColor 从colors中获取对应的color并返回
-func getColor(color string) string {
-	c, ok := colors[color]
-	if !ok {
-		c = "none"
-	}
-
-	return c
-}
-
 // SetDefaultColor 设置defaultColor
 // color为""时设置为黑色
 // 不会改变现有text内容的颜色
-// 颜色不存在时返回error
-func (l *ColorLabel) SetDefaultColor(color string) error {
+func (l *ColorLabel) SetDefaultColor(color string) {
 	if color == "" {
-		l.defaultColor = "black"
-		return nil
+		l.defaultColor = defaultStyle
+		return
 	}
 
-	c := getColor(color)
-	if c == "none" {
-		return ErrColorNotFound
-	}
-	l.defaultColor = color
-
-	return nil
+	l.defaultColor = fmt.Sprintf(colorStyle, color)
 }
 
 // ChangeColor 改变现有text的颜色
 // 并且设置defaultColor为新的颜色
-// color为""时设置为黑色
-// 颜色不存在时返回error
-func (l *ColorLabel) ChangeColor(color string) error {
-	if err := l.SetDefaultColor(color); err != nil {
-		return err
-	}
-
-	text, err := l.PureText()
-	if err != nil {
-		return err
-	}
-	l.SetDefaultColorText(text)
-
-	return nil
-}
-
-// PureText 获取纯text内容，不包括color部分
-// 不许label的text为空
-func (l *ColorLabel) PureText() (string, error) {
+// color为""时设置为defaultStyle
+func (l *ColorLabel) ChangeColor(color string) {
+	l.SetDefaultColor(color)
 	text := l.Text()
-	tmp := textMatcher.FindStringSubmatch(text)
-	if len(tmp) < 2 {
-		return "", ErrColorNotFound
-	}
-
-	return tmp[1], nil
+	l.SetDefaultColorText(text)
 }
 
 // SetColorText 用color显示新的text
-// color为""时显示黑色
-func (l *ColorLabel) SetColorText(text, color string) error {
-	if color == "black" {
-		l.SetText(text)
-		return nil
-	} else if color == "" {
-		if l.defaultColor != "" {
-			// 递归调用，下次调用时color不可能是空，所以一定可以设置颜色
-			// 不在这里直接使用SetDefaultColorText，避免调用链不清晰
-			return l.SetColorText(text, l.defaultColor)
-		}
-
-		return errors.New("no default color set")
+// color为""时显示defaultStyle
+func (l *ColorLabel) SetColorText(text, color string) {
+	var style string
+	if color == "" {
+		style = defaultStyle
+	} else {
+		style = fmt.Sprintf(colorStyle, color)
 	}
 
-	c := getColor(color)
-	if c == "none" {
-		return ErrColorNotFound
-	}
-
-	newText := fmt.Sprintf(c, text)
-	l.SetText(newText)
-
-	return nil
+	l.SetText(text)
+	l.SetStyleSheet(style)
 }
 
-// SetDefaultColorText 设置新的text值，并使其显示创建时指定的default color
-func (l *ColorLabel) SetDefaultColorText(text string) error {
-	return l.SetColorText(text, l.defaultColor)
+// SetDefaultColorText 设置新的text值，并使其显示设置的default color
+func (l *ColorLabel) SetDefaultColorText(text string) {
+	l.SetText(text)
+	l.SetStyleSheet(l.defaultColor)
+}
+
+// DropColor 去除自定义颜色，显示系统主题默认的颜色
+func (l *ColorLabel) DropColor() {
+	// 空字符串去除stylesheet
+	l.SetStyleSheet("")
+	l.defaultColor = ""
 }
