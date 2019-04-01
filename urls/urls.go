effaed384552f83815f576e006305706a324a332
apocelipes <seve3r@outlook.com>
2019-04-01 14:30:31 +0800

    update urls

--- urls/urls.go
+++ urls/urls.go
@@ -2,15 +2,15 @@ package urls
 
 const (
 	// RootPath 网站主URL
-	RootPath        = `https://sgchannel.me/`
+	RootPath = `https://sgchannel.cloud/`
 	// AuthPath 登录页面的URL
-	AccountPath        = RootPath + `clientarea.php`
+	AccountPath = RootPath + `clientarea.php`
 	// ServiceListPath 服务列表URL
 	ServiceListPath = AccountPath + `?action=services`
 	// LoginPath 登录验证的URL
-	LoginPath       = RootPath + `dologin.php`
+	LoginPath = RootPath + `dologin.php`
 	// InvoicePath 账单列表的URL
-	InvoicePath     = AccountPath + `?action=invoices`
+	InvoicePath = AccountPath + `?action=invoices`
 	// 测试代理的URL
-	ProxyTestPath   = `https://golang.org`
+	ProxyTestPath = `https://golang.org`
 )
